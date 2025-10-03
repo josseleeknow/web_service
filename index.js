1f03be6134ca5128f0606b297b8c21a4e3435c06
josseleeknow <josselinsotoc@gmail.com>
2025-10-03 13:56:15 -0700

    Actualizacion practica 2

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("¡Hola Mundo Soy Deyanira Josselin!");
+  const nombre = req.query.nombre || "Mundo";
+
+  res.json({
+    mensaje: `¡Hola ${nombre}, soy Deyanira Josselin!`,
+    status: "success"
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
